1b5fa960ecba70d56f478b7738aab5bab186589b
akshay-shelar303 <you@example.com>
2024-03-15 13:22:38 +0530

    initial commit

---
+++ reminderapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import Notification
+from .serializers import NotificationSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(["GET", "POST"])
+def notifyView(request):
+    serializer = NotificationSerializer(data=request.data)
+    if request.method == "POST":
+        serializer = NotificationSerializer(data=request.POST)
+
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
