916eaade6c71e7e00ee732e8582b42f4ebac43bc
akshay-shelar303 <you@example.com>
2024-03-21 13:49:16 +0530

    functional based views removed

--- reminderapp/views.py
+++ reminderapp/views.py
@@ -9,22 +9,6 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 
-@api_view(["GET", "POST"])
-def notifyView(request):
-    notifications = Notification.objects.all()
-    serializer = NotificationSerializer(notifications, many=True)
-    if request.method == "POST":
-        serializer = NotificationSerializer(data=request.data)
-
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
 class Notify(APIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication]
