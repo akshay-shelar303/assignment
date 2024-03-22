93428750e03876ad8583ed1818b1167dd99a676a
akshay-shelar303 <you@example.com>
2024-03-22 13:10:15 +0530

    removes fbv urls

--- reminderapp/urls.py
+++ reminderapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import Notify, UpdateNotify, notifyView
+from .views import Notify, UpdateNotify
+
 
 urlpatterns = [
-    path("reminder/", notifyView, name="reminder"),
     path("notify/", Notify.as_view(), name="notify"),
     path("updatenotify/<int:pk>/", UpdateNotify.as_view(), name="update"),
 ]
